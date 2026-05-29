a070755a70adc85cb9b29bfea4f78ffce2a91faa
Revati <your-email@example.com>
2026-05-29 15:36:04 +0530

    changes {29/5/2026} java code

---
+++ src/JavaCode.java
@@ -0,0 +1,107 @@
+import java.util.Scanner;
+
+public class JavaCode {
+
+    public static void main(String[] args) {
+
+      /* int a= 21;
+        System.out.println(a);
+        boolean b= true;
+        System.out.println(b);
+        char c='c';
+        System.out.println(c);*/
+
+       /* Scanner sc = new Scanner(System.in);
+        System.out.println("enter first value");
+       // int i=sc.nextInt();
+        String i = sc.next();
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("enter second value");
+       // int i1=sc1.nextInt();
+        String i1 = sc.next();
+        String i2=i+i1;
+        System.out.println(i2);*/
+
+       /* int a = 5;
+        int b = 6;*/
+
+        // int c = a++ + b;
+        //System.out.println(c);
+       /* int d = ++a + b;
+        System.out.println(d);*/
+       /* int e =a-- + b;
+        System.out.println(e);*/
+       /* int f = --a + b;
+        System.out.println(f);*/
+
+        /*System.out.println(a>b);//false
+        System.out.println(a==b);//false*/
+
+
+        /*boolean a= true;
+        boolean b =false;
+        System.out.println(""+(a&&b));
+
+        System.out.println(""+(a||b));
+        System.out.println(""+!(a&&b));*/
+
+
+      /*  String a= "Gaurav";
+      boolean b = a instanceof String;
+        System.out.println(b);*/
+
+ /*int i=1;
+while (i<5){
+    System.out.println(i);
+    i++;
+
+}*/
+
+        /*int i=2;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<5);*/
+      /*  int i=3;
+        for ( i=1;i<=5;i++){
+
+            System.out.println(i);
+            i+=2;
+
+        }*/
+      /*  for (int i = 1; i < 5; i++) {
+            for (int j = 2; j < 5; j++) {
+                System.out.println("*");
+            }
+            System.out.println("1");
+        }*/
+
+
+       /* int a[]={1,2,3,4,5,6,7};
+        for(int i :a){
+            System.out.println(i);
+        }*/
+
+        /*int i = 3;
+        for (i = 1; i <= 5; i++) {
+            if (i == 2) {
+                break;
+            }
+            System.out.println(i);
+            i += 2;
+
+        }*/
+
+        int i = 3;
+        for (i = 1; i <= 5; i++) {
+            if (i == 2) {
+                continue;
+            }
+            System.out.println(i);
+            i += 2;
+
+        }
+
+    }
+
+}
